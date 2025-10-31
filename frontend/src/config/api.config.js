a1287fc48632f920b2f549e4690c1e0d915bddd1
Suman Dey <140014618+sumandey2023@users.noreply.github.com>
2025-10-31 20:12:04 +0530

    `Update API base URL in api.config.js to include '/api' path`

--- frontend/src/config/api.config.js
+++ frontend/src/config/api.config.js
@@ -1,4 +1,4 @@
 // const API_BASE_URL = "http://localhost:3000/api";
-const API_BASE_URL = "https://rozgar-darpan-1.onrender.com/api";
+const API_BASE_URL = "https://rozgar-darpan-1.onrender.com";
 
 export default API_BASE_URL;
